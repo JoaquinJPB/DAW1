33d1b46b1524cb2a02454bac4697a765135370dc
abrandvelaz <abrandon@inerza.com>
2021-10-06 11:39:04 +0100

    registro

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function getEnviroment ($name) {
-        $file = file_get_contents(__DIR__ . "/env.json");
-        $json = json_decode($file, true);
-        return $json[$name];
-    }
-?>
--- partials/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer text-faded text-center py-5">
-  <div class="container">
-    <p class="m-0 small">Copyright &copy; AILA Motors 2021</p>
-  </div>
-</footer>
